83bc8cad3c1ddb27062a5f282aa02ab92c331afa
gaurang <gg270598@gmail.com>
2023-02-14 21:02:57 +0530

    added links

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 X_FRAME_OPTIONS = '*'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/',]
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/',"https://twitterclone-socialchatbox.gauranggupta6.repl.co"]
 
 
 
